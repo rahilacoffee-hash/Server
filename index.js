02c386c949c229db3d3f3f04fa5608f1d6055437
fred <abrahamfred123@gmail.com>
2026-07-27 17:13:25 +0100

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
